c9c49a13d6456c7da3a7aa21416f7f81a43a6359
TomRobert <benjamin.scheiwiller@hotmail.com>
2018-08-07 22:03:57 +0200

    version

--- TW-Calc.user.js
+++ TW-Calc.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name The-West Calc
-// @version 1.36
+// @version 1.37
 // @description The-West Battle Calc, Notepad, Battle stats, Duel Calc, Duel list, Craft list, Job list, Wardrobe, Tombola analyser
 // @author theTim, Tom Robert
 // @website http://tw-calc.net
@@ -71,7 +71,7 @@ window.TWCalc_inject = function () {
 
         window.TW_Calc = {
             scriptName: "The-West Calc",
-            version: "1.36",
+            version: "1.37",
             gameMAX: Game.version.toString(),
             author: ["MarcusJohnyEvans", "Tom Robert"],
             gameMIN: "1.36",
