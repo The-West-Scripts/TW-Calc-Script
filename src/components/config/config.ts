0e8d62a35a20b5fef2ee2ef7eb66c9c9b21d236b
Timotej Zatko <timi.zatko@gmail.com>
2020-12-17 22:10:39 +0100

    Disable birthday

--- src/components/config/config.ts
+++ src/components/config/config.ts
@@ -12,7 +12,7 @@ export class Config {
     public readonly showUntranslated = false;
     public readonly logPrefix = 'TW-Calc';
     public readonly birthday = {
-        enabled: true,
+        enabled: false,
         date: '2012/12/16',
     };
     public readonly cdn = '//westzz.innogamescdn.com/';
