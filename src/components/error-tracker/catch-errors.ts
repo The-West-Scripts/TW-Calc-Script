60c8fe04aad30933cd2e317b5b95ef8485e99986
Timotej Zatko <timi.zatko@gmail.com>
2021-01-09 19:41:42 +0100

    Add callback to ErrorTracker

--- src/components/error-tracker/catch-errors.ts
+++ src/components/error-tracker/catch-errors.ts
@@ -1,17 +1,43 @@
 import { Component } from '../component.types';
 
-export type CatchErrors = (target: Component, propertyKey: string | symbol, descriptor: PropertyDescriptor) => void;
+export type CatchErrors<TargetObject extends Component = Component> = (target: TargetObject, propertyKey: string | symbol, descriptor: PropertyDescriptor) => void;
 
-export function CatchErrors(component?: string): CatchErrors {
-    return function (_: Component, __: string | symbol, descriptor: PropertyDescriptor) {
+export interface CatchErrorsOptions<TargetObject extends Component> {
+    component?: string;
+    callback?: (object: TargetObject) => void;
+}
+
+export function CatchErrors(): CatchErrors;
+export function CatchErrors(component: string): CatchErrors;
+export function CatchErrors<TargetObject extends Component = Component>(options: CatchErrorsOptions<TargetObject>): CatchErrors<TargetObject>;
+export function CatchErrors<TargetObject extends Component = Component>(parameter?: string | CatchErrorsOptions<TargetObject>): CatchErrors<TargetObject> {
+    let component: string | undefined = undefined;
+    let callback: ((object: TargetObject) => void) | undefined = undefined;
+
+    if (typeof parameter === 'string') {
+        component = parameter;
+    } else if (typeof parameter !== 'undefined') {
+        ({ component, callback } = parameter);
+    }
+
+    return function (_: TargetObject, __: string | symbol, descriptor: PropertyDescriptor) {
         const originalMethod = descriptor.value;
 
-        descriptor.value = function (this: Component) {
+        descriptor.value = function (this: TargetObject) {
             try {
                 originalMethod.bind(this, ...Array.from(arguments))();
             } catch (e: any) {
                 const error = asErrorObject(e);
                 this.errorTracker.track(error, component);
+
+                if (typeof callback !== 'undefined') {
+                    try {
+                        callback(this);
+                    } catch (e: any) {
+                        const componentCallback = component ? `${component}.errorCallback` : 'errorCallback';
+                        this.errorTracker.track(asErrorObject(e), componentCallback);
+                    }
+                }
             }
         };
 
