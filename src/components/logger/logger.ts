bb82d10538bb00679837de017f5c231ee263bed0
Timotej Zatko <timi.zatko@gmail.com>
2021-02-27 14:11:15 +0100

    Add possibility to show logs in production env

--- src/components/logger/logger.ts
+++ src/components/logger/logger.ts
@@ -1,11 +1,16 @@
 import { Config } from '../config/config';
-import { ENV } from '../../constants';
 import { inject, singleton } from 'tsyringe';
 import { LogMethod } from './logger.types';
+import { Storage } from '../storage/storage';
+import { StorageKey } from '../storage/storage.types';
 
 @singleton()
 export class Logger {
-    constructor(@inject('window') private window: Window & { console: Console }, private config: Config) {}
+    constructor(
+        @inject('window') private window: Window & { console: Console },
+        private config: Config,
+        private storage: Storage,
+    ) {}
 
     error(...args: any[]): void {
         this.print('error', args);
@@ -13,7 +18,7 @@ export class Logger {
 
     log(...args: any[]): void {
         // log is disabled in the production
-        if ((ENV as string) !== 'prod') {
+        if (this.canShowLogs()) {
             this.print('log', args);
         }
     }
@@ -24,9 +29,17 @@ export class Logger {
 
     debug(...args: any[]): void {
         // debug is disabled in the production
-        if (this.config.env !== 'prod') {
+        if (this.canShowLogs()) {
             this.print('debug', args);
         }
+    }
+
+    private canShowLogs(): boolean {
+        return this.config.env !== 'prod' || this.isDebugEnabled();
+    }
+
+    private isDebugEnabled(): boolean {
+        return this.storage.getObject(StorageKey.debug, true);
     }
 
     private print(method: LogMethod, args: any[]): void {
--- src/components/storage/storage.types.ts
+++ src/components/storage/storage.types.ts
@@ -1,4 +1,5 @@
 export enum StorageKey {
+    debug = 'DEBUG', // when set, debug logs will be shown
     languagePack = 'LANGUAGE_PACK', // renamed from LANG_PACK
     languagePackLastUpdate = 'LANGUAGE_PACK_LAST_UPDATE', // renamed from LANG_PACK_LAST_UPDATE
     languagePackReset = 'LANGUAGE_PACK_RESET', // renamed from LANG_PACK_RESET
