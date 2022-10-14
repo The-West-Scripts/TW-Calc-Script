d60540a24881c2d759fb8700f652fc5615f128ec
Timotej Zatko <timi.zatko@gmail.com>
2022-10-14 15:24:30 +0200

    feat: update changelog

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,6 +7,6 @@ VERSION="$(echo "$REF" | cut -d '/' -f3)"
 curl \
   --form "apiKey=$TW_CALC_API_KEY" \
   --form "version=$VERSION" \
-  --form "updateInfo=Update $VERSION" \
+  --form "updateInfo=See <a href='https://github.com/The-West-Scripts/TW-Calc-Script/releases'>releases</a>." \
   --form 'userscript=@./dist/TW_Calc.user.js' \
   https://tw-calc.net/service/userscript-publish
